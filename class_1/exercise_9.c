e07d48ab30a6256c73b49b6c5e7deb6a872a1d87
Abid Hasan Tonmoy <95616443+abidtonmoy@users.noreply.github.com>
2022-05-29 01:30:43 +0600

    Update exercise_9.c

--- class_1/exercise_9.c
+++ class_1/exercise_9.c
@@ -7,22 +7,22 @@ int main()
 	scanf("%d",&number);
 	
 	a=number/10;
-	d4=number%10;
+	d1=number%10;
 	b=a/10;
-	d3=a%10;
+	d2=a%10;
 	c=b/10;
-	d2=b%10;
+	d3=b%10;
 	d=c/10;
-	d1=c%10;
+	d4=c%10;
 	
 	
 	
-////	printf("First Number:%d\n: ",d1);
-///	printf("Second Number:%d",d4); 
+	printf("First Number:%d\n: ",d1);
+	printf("last Number:%d\n",d4); 
 	
 	sum=d1+d4;
 	
-	printf("Sum of first %d and last digit %  is %d",d1,d4,sum);
+	printf("Sum of first and last digit is = %d",sum);
 	
 	return 0;
 	
